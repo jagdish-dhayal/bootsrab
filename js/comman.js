af099d8962e60751acc80684d48c7741de473da1
jagdish-dhayal <jpdhayaltec@gmail.com>
2023-03-18 18:16:54 +0530

    add file

---
+++ js/comman.js
@@ -0,0 +1,41 @@
+// output show 
+
+// alert('test');
+// alert('test');
+// alert('test');
+// alert('test');
+// alert('test');
+
+// document.write('my name is javed khan');
+// document.write('my name is javed khan');
+// document.write('my name is javed khan');
+// document.write('my name is javed khan');
+// document.write('my name is javed khan');
+// document.write('my name is javed khan');
+// document.write('my name is javed khan');
+// document.write('my name is javed khan');
+// document.write('my name is javed khan');
+// document.write('my name is javed khan');
+
+
+// console.log('sjjjs');
+function test(){
+    // var 
+    
+
+    // let 
+    // const 
+ var x=   prompt('enter number')
+    alert(x)
+    document.getElementById('test').innerHTML="my name is javed ";
+}
+
+
+
+// let x=10;
+// if (x > 5) {
+//     console.log('true');
+    
+// } else {
+//     console.log('false');
+// }
